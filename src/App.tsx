fe52ef42cb3e3238a9fb6ea8dedca9994dc8aeac
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-09-12 17:59:40 -0300

    password

--- src/App.tsx
+++ src/App.tsx
@@ -26,20 +26,38 @@ function App() {
 
   const [documentHeight, setDocumentHeight] = useState(0);
 
-useEffect(() => {
-  const updateHeight = () => {
-    setDocumentHeight(document.documentElement.scrollHeight);
-  };
-  
-  // Inicializar altura y agregar listener para cambios de tamaño
-  updateHeight();
-  window.addEventListener('resize', updateHeight);
+  useEffect(() => {
+    const updateHeight = () => {
+      setDocumentHeight(document.documentElement.scrollHeight);
+    };
 
-  // Cleanup
-  return () => {
-    window.removeEventListener('resize', updateHeight);
+    // Inicializar altura y agregar listener para cambios de tamaño
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+    };
+  }, []);
+
+  const [password, setPassword] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const correctPassword = 'asf756'; // Define tu contraseña aquí
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
   };
-}, []);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (password === correctPassword) {
+      setIsAuthenticated(true);
+    } else {
+      alert('Contraseña incorrecta');
+    }
+  };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -57,6 +75,26 @@ useEffect(() => {
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  if (!isAuthenticated) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <h2 className="text-white text-2xl mb-4">Ingresa la contraseña</h2>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="p-2 mb-4 border border-gray-400 rounded"
+            placeholder="Contraseña"
+          />
+          <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div className="App min-h-screen bg-[#1B1B1B]">
@@ -79,9 +117,7 @@ useEffect(() => {
           <div ref={redesSocialesRef}>
             <Home />
           </div>
-          
-          
-          <div className="h-[100px]" />
+
           <NetworksCard />
           <div ref={sobreMiRef}>
             <QuienSoy />
