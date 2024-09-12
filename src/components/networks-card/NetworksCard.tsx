54c4d540e8ab4023422c5288c3fbaac3a7af5c8c
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-09-12 17:28:57 -0300

    bottom 0

--- src/components/networks-card/NetworksCard.tsx
+++ src/components/networks-card/NetworksCard.tsx
@@ -5,7 +5,7 @@ import Facebook from "../../assets/FACEBOOK.png";
 
 export default function NetworksCard() {
   return (
-    <div className="bg-white flex flex-row items-center justify-center gap-10 flex-grow w-full rounded-[25px] h-[173px] absolute z-20 bottom-[150px] lg:bottom-0 lg:gap-16">
+    <div className="bg-white flex flex-row items-center justify-center gap-10 flex-grow w-full rounded-[25px] h-[173px] absolute z-20 bottom-0 lg:bottom-0 lg:gap-16">
       <a href="https://www.instagram.com/adriansfeir/" target="_blank" rel="noopener noreferrer">
         <div className="w-[51px] lg:w-16">
           <img className="w-full" src={Instagram} alt="Instagram" />
