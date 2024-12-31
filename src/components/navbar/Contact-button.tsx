36e8b6fc63efbc7442b2cf4d178989e9cfd5df08
Ismael Fernandez <ismaelfdc2004@gmail.com>
2024-12-31 14:08:59 -0300

    tilde contacto boton

--- src/components/navbar/Contact-button.tsx
+++ src/components/navbar/Contact-button.tsx
@@ -2,11 +2,14 @@ import { useContext } from "react";
 import { ScrollContext } from "../../App";
 
 export default function ContactButton() {
+  const { scrollToComponent, refs } = useContext(ScrollContext);
 
-    const { scrollToComponent, refs } = useContext(ScrollContext)
-
-    return (
-        <button onClick={() => scrollToComponent(refs.contactoRef)} className="button-gradient rounded-[8px] h-[36px] w-[96px] text-white">Contácto</button>
-    )
-  }
-  
+  return (
+    <button
+      onClick={() => scrollToComponent(refs.contactoRef)}
+      className="button-gradient rounded-[8px] h-[36px] w-[96px] text-white"
+    >
+      Contacto
+    </button>
+  );
+}
